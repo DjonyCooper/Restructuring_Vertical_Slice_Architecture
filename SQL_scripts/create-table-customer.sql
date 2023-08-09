47d8add15d8b4b525fb017c0ec4cb0d5976e6679
DJC on Sweet Island <shampenyon@gmail.com>
2023-08-09 21:48:54 +0300

    DAO

---
+++ SQL_scripts/create-table-customer.sql
@@ -0,0 +1,8 @@
+USE DAO;
+CREATE TABLE customer(
+    id int NOT NULL PRIMARY KEY AUTO_INCREMENT COMMENT 'Primary Key',
+    customer_name VARCHAR(255) NOT NULL,
+    city_id int NOT NULL,
+    CONSTRAINT name_gt_2_char_2 CHECK (CHAR_LENGTH(customer_name) > 2),
+    CONSTRAINT city_relationship FOREIGN KEY (city_id) REFERENCES city(id)
+) DEFAULT CHARSET UTF8MB4 COMMENT '';
